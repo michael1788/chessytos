df2edefb3273408c040150ae9e9eeefdd8b4d25e
michael1788 <mrgmoret@gmail.com>
2025-05-11 19:18:33 -0400

    Initial Commit

---
+++ chessytos/chessytosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessytosApp.swift
+//  chessytos
+//
+//  Created by Michaël Moret on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct chessytosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chessytosTests/chessytosTests.swift
@@ -0,0 +1,17 @@
+//
+//  chessytosTests.swift
+//  chessytosTests
+//
+//  Created by Michaël Moret on 5/11/25.
+//
+
+import Testing
+@testable import chessytos
+
+struct chessytosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
